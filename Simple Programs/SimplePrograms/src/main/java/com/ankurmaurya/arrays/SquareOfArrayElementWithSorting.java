2da535aa716495f231a6d3b81999c1e42e7b5a71
Ankur Maurya <100197611+ankurkmaurya@users.noreply.github.com>
2023-04-26 15:53:58 +0530

    Added SquareOfArrayElementWithSorting
    Program Square Array Elements and Sort.

---
+++ Simple Programs/SimplePrograms/src/main/java/com/ankurmaurya/arrays/SquareOfArrayElementWithSorting.java
@@ -0,0 +1,49 @@
+
+package com.ankurmaurya.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author Ankur.Mourya
+ */
+public class SquareOfArrayElementWithSorting {
+
+    public static void main(String[] args) {
+        System.out.println("Square Array Elements and Sort");
+        int[] dataValues = {3, 6, 5, -1, -3, 2, 7, 1, 0};
+
+        String dispData = IntStream.of(dataValues).mapToObj(Integer::toString)
+                .collect(Collectors.joining(",", "[", "]"));
+        System.out.println("Data : " + dispData);
+
+        System.out.println("Approach : 1 (No Duplicates, Tree Set)");
+        Set<Integer> dataSet = new TreeSet<>();
+        for (int i = 0; i < dataValues.length; i++) {
+            dataSet.add(dataValues[i] * dataValues[i]);
+        }
+        System.out.println(dataSet);
+
+        System.out.println("Approach : 2 (With Duplicates, Array List)");
+        List<Integer> dataList = new ArrayList<>();
+        List<Integer> dataList1;
+        for (int i = 0; i < dataValues.length; i++) {
+            dataList.add(dataValues[i] * dataValues[i]);
+        }
+        System.out.println("Squared : " + dataList);
+        dataList1 = dataList.stream().collect(Collectors.toList());
+
+        Collections.sort(dataList);
+        Collections.sort(dataList1, (e1, e2) -> e2.compareTo(e1));
+        System.out.println("Ascending Sorted   : " + dataList);
+        System.out.println("Descending Sorted  : " + dataList1);
+
+    }
+
+}
